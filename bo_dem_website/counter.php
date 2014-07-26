251ed5656059768461886f8e4bf6d18a51b1a2e0
tronghuan <huandt92@gmail.com>
2014-07-27 00:50:58 +0700

    Revert "tao bo dem website"
    This reverts commit dfedfb0c1774fb4b9b7f8b066f23364854b677c3.

--- bo_dem_website/counter.php
+++
@@ -1,8 +0,0 @@
-<?php
-$viewss = file("counterlog.txt");
-$views = $viewss[0];
-$views++;
-$fp = fopen("counterlog.txt", "w");
-fwrite($fp, $views);
-fclose($fp);
-?>
--- bo_dem_website/result.php
+++
@@ -1,4 +0,0 @@
-<?php
-include("counter.php");
-print $views;
-?>
