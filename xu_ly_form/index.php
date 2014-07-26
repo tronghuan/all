8726477cc5924ad14893cf0e9684385310bc2b59
tronghuan <huandt92@gmail.com>
2014-07-27 00:20:44 +0700

    may qua

---
+++ xu_ly_form/index.php
@@ -0,0 +1,6 @@
+<form action="welcome.php" name="dauvao" method="post">
+	<p>Username: <input type="text" name="username" value="" placeholder="Hay nhap username vao..." /></p>
+	<p>Password: <input type="password" name="password" value="" placeholder="Hay nhap password vao..." /></p>
+	<p><input type="submit" name="submit" value="Login"></p>
+
+</form>
---
+++ xu_ly_form/welcome.php
@@ -0,0 +1,4 @@
+<?php
+if($_POST['username'] == 'admin' && $_POST['password'] == '12345') echo "welcome admin!";
+else echo "Nhap username va password sai!";
+?>
